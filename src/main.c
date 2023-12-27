3e4402e0b0c68fb0c7b97f5b78f13982e4d7d1e6
Remco Ritskes <7085414+Nerdeose@users.noreply.github.com>
2023-12-27 14:45:14 +0100

    Blink LED with IO expander
    LED blinks on GPIOB0.
    Initialising the IO expander is done by dissabling sequentiakl writes.
    Opcode is set to 01000001 with the last bit set 1 for write.
    The hardwired adress is disabled and pins should be grounded.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,25 @@
 // Created by remco on 26-12-23.
 //
 
+#include <util/delay.h>
+
+#include "spi.h"
+#include "mcp23s17.h"
 #include "main.h"
 
 int main(void)
 {
+    SPI_MasterInit();
+    IO_Init();
+    _delay_ms(500);
+
+    while(1)
+    {
+        IO_Write(0x13, 0x01);
+        _delay_ms(1000);
+        IO_Write(0x13, 0x00);
+        _delay_ms(1000);
+    }
+
     return 0;
 }
--- src/spi.h
+++ src/spi.h
@@ -10,5 +10,14 @@
 #define DD_MOSI PB3
 #define DD_MISO PB4
 #define DD_SCK PB5
+#define CS_PORT PORTB
+#define CS_PIN  PB2
+
+void SPI_MasterInit(void);
+void SPI_MasterTransmit(char data);
+void SPI_SlaveInit(void);
+char SPI_SlaveReceive(void);
+void SPI_ChipSelectStart(void);
+void SPI_ChipSelectStop(void);
 
 #endif //ARDUINOTEMPGRAPH_SPI_H
